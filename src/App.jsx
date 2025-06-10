78b32a2ef39e80daf5bcd3ba02cfb9c213e221ab
Gianna Ysabel Capino Besmonte <gbesmont@usc.edu>
2025-06-10 14:39:30 -0700

    sorting

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   //modal handling
   const [modal, setModal] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sort, setSort] = useState('none');
 
   function HandleSearchToggle(e){
     if (e.target.checked){
@@ -16,6 +17,19 @@ const App = () => {
     }
     else {
       setIsSearch(false);
+    }
+  }
+  function HandleSort(e){
+    switch (e.target.value){
+      case 'name':
+        setSort('name');
+        break;
+      case 'dates':
+        setSort('dates');
+        break;
+      case 'votes':
+        setSort('votes');
+        break;
     }
   }
 
@@ -31,9 +45,18 @@ const App = () => {
           <input type="checkbox" id="searchToggle" name="searchToggle" onClick={HandleSearchToggle}></input>
           <label htmlFor="searchToggle">Toggle</label>
         </div>
+        <div className="sort-bar">
+            <select name="sort" onChange={HandleSort}>
+                <option value="none">Sort By</option>
+                <option value="name">Title (A-Z)</option>
+                <option value="dates">Release Date (newest)</option>
+                <option value="votes">Vote Average (highest)</option>
+             </select>
+         </div>
       </header>
       <main>
-        {isSearch ? <Search setModal={setModal} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/> : <NewMovieList setModal={setModal} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>}
+        {isSearch ? <Search setModal={setModal} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/> :
+        <NewMovieList setModal={setModal} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} sort={sort}/>}
       </main>
       <footer></footer>
     </div>
